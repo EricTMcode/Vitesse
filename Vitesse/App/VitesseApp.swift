fa78276cd97d927bc7f5d6d15edd3abcf042c139
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-09 15:59:13 +0100

    first commit

---
+++ Vitesse/App/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Eric on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
