9a664f1de5d5c0d4b9c459a59c371b9d2cd3a715
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-09 16:31:09 +0100

    Create HTTPCLient to parse data

--- Vitesse/Shared/HTTPClient.swift
+++ Vitesse/Shared/HTTPClient.swift
@@ -13,7 +13,7 @@ protocol HTTPClientProtocol {
 }
 
 class HTTPClient: HTTPClientProtocol {
-    func fetchData<T>(_ endpoint: APIEndpoint) async throws -> T {
+    func fetchData<T: Codable>(_ endpoint: APIEndpoint) async throws -> T {
         guard let url = endpoint.request else {
             throw APIError.invalidURL
         }
@@ -23,11 +23,52 @@ class HTTPClient: HTTPClientProtocol {
         guard let httpResponse = response as? HTTPURLResponse else {
             throw APIError.networkError
         }
+
+        try httpResponse.validate(data: data)
+
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw APIError.decodingError("Decoding error: \(error)")
+        }
     }
     
     func perform(_ endpoint: APIEndpoint) async throws {
-        <#code#>
-    }
-    
+        guard let url = endpoint.request else {
+            throw APIError.invalidURL
+        }
 
+        let (data, response) = try await URLSession.shared.data(for: url)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw APIError.invalidResponse
+        }
+
+        try httpResponse.validate(data: data)
+    }
+}
+
+extension HTTPURLResponse {
+    func validate(data: Data) throws {
+        switch self.statusCode {
+        case 200...299:
+            return
+
+        case 401:
+            throw APIError.invalidCredentials
+
+        case 400...499:
+            let message = String(data: data, encoding: .utf8) ?? "Request error"
+            throw APIError.serverError(message)
+
+        case 500:
+            throw APIError.serverError("RegistrationFailed: The email is already in use.")
+
+        case 500...599:
+            throw APIError.serverError("Server error, please try again later.")
+
+        default:
+            break
+        }
+    }
 }
