6504c83b29fa2f4e04edfd6e3cfc11c244aa169e
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-11 10:36:09 +0100

    Create MockLoginService for tests

---
+++ VitesseTests/Authentication/MockLoginService.swift
@@ -0,0 +1,26 @@
+//
+//  MockLoginService.swift
+//  VitesseTests
+//
+//  Created by Eric on 11/01/2026.
+//
+
+import Foundation
+
+class MockLoginService: LoginServiceProtocol {
+    var resultToReturn: Result<AuthResonse, Error>?
+    var lastReceivedRequest: LoginRequest?
+
+    func login(with request: LoginRequest) async throws -> AuthResonse {
+        lastReceivedRequest = request
+
+        switch resultToReturn {
+        case .success(let response):
+            return response
+        case .failure(let error):
+            throw error
+        case nil:
+            fatalError("Result to return was not set in the test setup")
+        }
+    }
+}
