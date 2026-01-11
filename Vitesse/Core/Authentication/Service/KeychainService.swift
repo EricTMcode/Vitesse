431c05498ae1e1e9a1e852453eed552aa8473876
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-11 17:47:32 +0100

    Create KeychainHelper to store the JWT Token with apple security.

---
+++ Vitesse/Core/Authentication/Service/KeychainService.swift
@@ -0,0 +1,55 @@
+//
+//  KeychainService.swift
+//  Vitesse
+//
+//  Created by Eric on 11/01/2026.
+//
+
+import Security
+import Foundation
+
+class KeychainHelper {
+    static let shared = KeychainHelper()
+    private let service = "com.eric.Vitesse"
+    
+    // Save data securely
+    func save(_ data: Data, account: String) {
+        let query = [
+            kSecValueData: data,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account
+        ] as [CFString: Any]
+
+        // Add to keychain (delete first to ensure we overwrite)
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+
+    // Read data securely
+    func read(account: String) -> Data? {
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword,
+            kSecReturnData: true
+        ] as [CFString: Any]
+
+        var result: AnyObject?
+        SecItemCopyMatching(query as CFDictionary, &result)
+
+        return result as? Data
+    }
+
+    // Delete data
+    func delete(account: String) {
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword
+        ] as [CFString: Any]
+
+        SecItemDelete(query as CFDictionary)
+    }
+}
+
