6c431005b764f16543f6853fa83d9664b48c9e28
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-14 13:35:34 +0100

    Add NavigationTitle

--- Vitesse/Core/Candidate/View/CandidatsListView.swift
+++ Vitesse/Core/Candidate/View/CandidatsListView.swift
@@ -13,37 +13,30 @@ struct CandidatsListView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                List {
+                    ForEach(viewModel.candidats) { candidat in
+                        VStack(alignment: .leading) {
+                            Text("\(candidat.firstName) \(candidat.lastName)")
+                        }
+                    }
+                }
+                .listStyle(.plain)
+
                 Button("Logout") {
                     loginViewModel.logout()
                 }
                 .buttonStyle(.bordered)
-
-                List(viewModel.candidats) { candidat in
-                    VStack(alignment: .leading) {
-                        Text("\(candidat.firstName) \(candidat.lastName)")
-                        Text(candidat.email)
-                    }
-                }
             }
+            .navigationTitle("Candidtats")
+            .navigationBarTitleDisplayMode(.inline)
+
             .task {
                 await viewModel.getCandidats()
             }
         }
-
-//        VStack(spacing: 30) {
-//            Text("Welcome! 🎉")
-////
-////            Text("Is Admin ? \(authManager.isAdmin ? "Yes" : "No")")
-//
-//            Button("Logout") {
-//                viewModel.logout()
-//            }
-//            .buttonStyle(.bordered)
-//        }
-//        .padding()
     }
 }
 
 #Preview {
-    CandidatsListView(loginViewModel: LoginViewModel(), viewModel: CandidatsListViewModel())
+    CandidatsListView(loginViewModel: LoginViewModel(), viewModel: CandidatsListViewModel(service: MockCandidateService()))
 }
