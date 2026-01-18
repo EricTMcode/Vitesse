0d096822955855734591d2a612c319a14efcedc9
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-18 21:02:35 +0100

    Add register success tests

--- VitesseTests/Authentication/RegisterViewModelTests.swift
+++ VitesseTests/Authentication/RegisterViewModelTests.swift
@@ -28,4 +28,25 @@ final class RegisterViewModelTests: XCTestCase {
     func testInit() {
         XCTAssertNotNil(viewModel, "The view model should not be nil")
     }
+
+    func test_register_success_sets_Registration_Success() async {
+        // Given
+        mockService.resultToReturn = .success(())
+
+        viewModel.registerRequest = User(
+            firstName: "Eric",
+            lastName: "Dupont",
+            email: "eric@test.com",
+            password: "Password1!",
+            confirmPassword: "Password1!"
+        )
+
+        // When
+        await viewModel.register()
+
+        // Then
+        XCTAssertTrue(viewModel.isRegistrationSuccessful)
+        XCTAssertNil(viewModel.errorMessage)
+        XCTAssertEqual(mockService.lastReveivedUser?.email, "eric@test.com")
+    }
 }
