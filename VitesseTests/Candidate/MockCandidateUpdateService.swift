4f886a64f3dd8b571e73460989900058cd9e036f
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-23 11:04:08 +0100

    Create MockCandidateUpdateService

---
+++ VitesseTests/Candidate/MockCandidateUpdateService.swift
@@ -0,0 +1,37 @@
+//
+//  MockCandidateUpdateService.swift
+//  VitesseTests
+//
+//  Created by Eric on 23/01/2026.
+//
+
+import Foundation
+
+final class MockCandidateUpdateService: CandidateUpdateServiceProtocol {
+    private(set) var updateCandidateCalled = false
+    private(set) var toggleFavoriteCalled = false
+
+    private(set) var updatedCandidate: Candidate?
+    private(set) var toggledFavoriteId: String?
+
+    var updateCandidateError: Error?
+    var toggleFavotiteError: Error?
+
+    func updateCandidate(data: Candidate) async throws {
+        updateCandidateCalled = true
+        updatedCandidate = data
+
+        if let error = updateCandidateError {
+            throw error
+        }
+    }
+
+    func toggleFavorite(id: String) async throws {
+        toggleFavoriteCalled = true
+        toggledFavoriteId = id
+
+        if let error = toggleFavotiteError {
+            throw error
+        }
+    }
+}
