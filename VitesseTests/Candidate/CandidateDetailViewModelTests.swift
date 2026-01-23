b9ba9f023f873ae56b462b4bf9c508ff80b40b70
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-23 11:23:23 +0100

    Add test startEditing and cancelEditing

--- VitesseTests/Candidate/CandidateDetailViewModelTests.swift
+++ VitesseTests/Candidate/CandidateDetailViewModelTests.swift
@@ -28,5 +28,26 @@ final class CandidateDetailViewModelTests: XCTestCase {
         super.tearDown()
     }
 
+    func testInit() {
+        XCTAssertNotNil(viewModel, "The vie model should not be nil")
+    }
 
+    func test_startEditing_setsDraftAndEditingState() {
+        viewModel.startEditing()
+
+        XCTAssertTrue(viewModel.isEditing)
+        XCTAssertNotNil(viewModel.draftCandidate)
+        XCTAssertEqual(viewModel.draftCandidate?.email, viewModel.candidate.email)
+    }
+
+    func test_cancelEditing_resetsDraftAndError() {
+        viewModel.startEditing()
+        viewModel.errorMessage = "Error"
+
+        viewModel.cancelEditing()
+
+        XCTAssertFalse(viewModel.isEditing)
+        XCTAssertNil(viewModel.draftCandidate)
+        XCTAssertNil(viewModel.errorMessage)
+    }
 }
