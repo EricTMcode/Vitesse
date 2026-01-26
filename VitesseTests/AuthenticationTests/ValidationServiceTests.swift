3b91c17c23671c0f6ce8e2db4d93502fd343d381
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-26 14:01:26 +0100

    Create ValidationServiceTests

---
+++ VitesseTests/AuthenticationTests/ValidationServiceTests.swift
@@ -0,0 +1,21 @@
+//
+//  ValidationServiceTests.swift
+//  VitesseTests
+//
+//  Created by Eric on 26/01/2026.
+//
+
+import XCTest
+@testable import Vitesse
+
+final class ValidationServiceTests: XCTestCase {
+
+    private var service: ValidationService!
+
+    override func setUp() {
+        super.setUp()
+        service = ValidationService()
+    }
+
+    
+}
