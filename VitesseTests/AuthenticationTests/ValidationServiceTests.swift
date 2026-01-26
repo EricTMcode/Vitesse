e91bb84890423467a4e5a9eb583c503765208cbb
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-26 14:06:32 +0100

    Add test validateEmail should be valid and invalid

--- VitesseTests/AuthenticationTests/ValidationServiceTests.swift
+++ VitesseTests/AuthenticationTests/ValidationServiceTests.swift
@@ -17,5 +17,34 @@ final class ValidationServiceTests: XCTestCase {
         service = ValidationService()
     }
 
-    
+    func test_validateEmail_validEmails() {
+        let validEmails = [
+            "test@vitesse.com",
+            "eric.doe@mail.co",
+            "user+alias@gmail.com",
+            "a.b_c-d%1@test-domain.org"
+        ]
+
+        validEmails.forEach {
+            XCTAssertTrue(service.validateEmail($0), "Expected \($0) to be valid")
+        }
+    }
+
+    func test_validateEmail_invalidEmails() {
+        let invalidEmails = [
+            "",
+            "test",
+            "test@",
+            "@test.com",
+            "test@test",
+            "test@test.",
+            "test@.com",
+            "test@com",
+            "test@@test.com"
+        ]
+
+        invalidEmails.forEach {
+            XCTAssertFalse(service.validateEmail($0), "Expected \($0) to be invalid")
+        }
+    }
 }
