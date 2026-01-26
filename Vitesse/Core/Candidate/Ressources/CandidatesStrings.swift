d4b9c86ff7154db05e9306473bafd2a086cb7a8e
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-26 14:23:56 +0100

    Refactor candidatesStrings

--- Vitesse/Core/Candidate/Ressources/CandidatesStrings.swift
+++ Vitesse/Core/Candidate/Ressources/CandidatesStrings.swift
@@ -9,31 +9,31 @@ import Foundation
 
 enum CandidatesStrings {
     enum Common {
-        static let edit = NSLocalizedString("editer", comment: "")
-        static let cancel = NSLocalizedString("annuler", comment: "")
-        static let delete = NSLocalizedString("effacer", comment: "")
-        static let saved = NSLocalizedString("enregistrer", comment: "")
+        static let edit = NSLocalizedString("edit", comment: "")
+        static let cancel = NSLocalizedString("cancer", comment: "")
+        static let delete = NSLocalizedString("delete", comment: "")
+        static let saved = NSLocalizedString("save", comment: "")
     }
 
     enum CandidatesList {
         static let title = NSLocalizedString("Candidats", comment: "")
 
-        static let searchCandidate = NSLocalizedString("Rechercher un candidat", comment: "")
-        static let loadingCandidates = NSLocalizedString("Chargement des candidats", comment: "")
-        static let noCandidatesAvailable = NSLocalizedString("Aucun Candidats", comment: "")
-        static let addACandidate = NSLocalizedString("Merci d'ajouter un candidat", comment: "")
-        static let reloadView = NSLocalizedString("Recharger", comment: "")
+        static let searchCandidate = NSLocalizedString("Search a candidat", comment: "")
+        static let loadingCandidates = NSLocalizedString("Loading of candidats", comment: "")
+        static let noCandidatesAvailable = NSLocalizedString("No candidates available", comment: "")
+        static let addACandidate = NSLocalizedString("Please add a candidate", comment: "")
+        static let reloadView = NSLocalizedString("Reload", comment: "")
         static let candidate = NSLocalizedString("Candidate", comment: "")
     }
 
     enum CandidateDetail {
         static let isAdmin = "isAdmin"
         static let linkedin = NSLocalizedString("linkedIn", comment: "")
-        static let goOnLinkedin = NSLocalizedString("Voir sur LinkedIn", comment: "")
+        static let goOnLinkedin = NSLocalizedString("Go on LinkedIn", comment: "")
         static let note = NSLocalizedString("note", comment: "")
-        static let noLinkedInAccount = NSLocalizedString("Pas de compte LinkedIn", comment: "")
-        static let phone = NSLocalizedString("téléphone", comment: "")
-        static let noPhone = NSLocalizedString("Ajoutez un numéro", comment: "")
+        static let noLinkedInAccount = NSLocalizedString("No LinkedIn account", comment: "")
+        static let phone = NSLocalizedString("phone", comment: "")
+        static let noPhone = NSLocalizedString("Add a number", comment: "")
         static let email = NSLocalizedString("email", comment: "")
     }
 }
